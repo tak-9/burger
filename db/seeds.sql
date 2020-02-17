2ef0115e1850c166a43fe9aaccc587c41767d5a5
Takuji Okubo <takuji.okubo@gmail.com>
2020-02-18 10:49:16 +1100

    Initial upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peri Peri Chickenx', false);
